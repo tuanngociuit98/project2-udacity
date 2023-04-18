48b1cfed08d2b83055a8d1b27884e7c487a53607
tuanngociuit98 <tuanngociuit98@gmail.com>
2023-04-18 14:50:20 +0700

    chawnge permission

--- make_prediction.sh
+++ make_prediction.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PORT=8000
+PORT=5000
 echo "Port: $PORT"
 
 # POST method predict
@@ -25,4 +25,4 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST http://localhost:$PORT/predict
+     -X POST https://udacity-webapp.scm.azurewebsites.net:$PORT/predict
