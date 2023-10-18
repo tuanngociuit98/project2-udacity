6e32663eb7dfffd660359ecb596c1da304917f9b
tuanngociuit98 <tuanngociuit98@gmail.com>
2023-10-18 13:39:55 +0700

    test

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, jsonify
-from flask.logging import create_logger
+from flask  import Flask, request, jsonify, g
+from flask.logging import create_logger  
 import logging
 
 import pandas as pd
-import joblib
-from sklearn.preprocessing import StandardScaler
+from sklearn.externals import joblib  
+#import joblib
+from sklearn.preprocessing import StandardScaler 
 
 app = Flask(__name__)
 LOG = create_logger(app)
@@ -26,12 +27,36 @@ def home():
 # TO DO:  Log out the prediction value
 @app.route("/predict", methods=['POST'])
 def predict():
-    # Performs an sklearn prediction
+    """Performs an sklearn prediction
+
+    input looks like:
+            {
+    "CHAS":{
+      "0":0
+    },
+    "RM":{
+      "0":6.575
+    },
+    "TAX":{
+      "0":296.0
+    },
+    "PTRATIO":{
+       "0":15.3
+    },
+    "B":{
+       "0":396.9
+    },
+    "LSTAT":{
+       "0":4.98
+    }
+
+    result looks like:
+    { "prediction": [ 20.35373177134412 ] }
+
+    """
+
     try:
-        # Load pretrained model as clf. Try any one model. 
-        # clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
-        # clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
-        clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
+        clf = joblib.load("boston_housing_prediction.joblib")
     except:
         LOG.info("JSON payload: %s json_payload")
         return "Model not loaded"
--- command.sh
+++ command.sh
@@ -1,1 +1,1 @@
-az webapp up --name mywebapp19042023 --resource-group Azuredevops --runtime "PYTHON:3.7"
+az webapp up --name mywebapp18102023 --resource-group Azuredevops --runtime "PYTHON:3.8"
