7b24e666a3ee0ea5d9d2e5699b0603d2f797a7f9
tuanngociuit98 <tuanngociuit98@gmail.com>
2023-10-28 20:21:55 +0700

    change make predict

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://project2-udacity.azurewebsites.net:$PORT/predict 
+     -X POST https://mywebapp28102023.azurewebsites.net/:$PORT/predict 
      #your application name <yourappname>goes here
